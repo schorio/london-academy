4acd4ef39a066870baf7aa0ce902f4a474efb44e
schorio <schorioignace@gmail.com>
2023-05-14 18:58:18 +0300

    categorie : creation du fonction de modif vers bdd

--- includes/function/liste/f_categorie.php
+++ includes/function/liste/f_categorie.php
@@ -24,4 +24,23 @@
 			echo "<script>alert('Une erreur s'est survenue');</script>";
 		}
 	}
+
+
+    if(isset($_POST['modifier_cat'])){
+		$modifier_id = $_POST['modifier_id'];
+		$n_libelle_cat = $_POST['n_libelle_cat'];
+		$n_observation_cat = $_POST['n_observation_cat'];
+		$sql = "UPDATE categorie SET 
+			libelle_cat='$n_libelle_cat',
+			observation_cat='$n_observation_cat'
+			WHERE id_cat='$modifier_id' ";
+		if ($conn->query($sql) === TRUE) {
+			echo '<script>window.location.href="/epn/liste/categorie/categorie.php"</script>';
+		} else {
+			echo "<script>alert('Une erreur s'est survenue');</script>";
+		}
+	}
+
+
+
 ?>
