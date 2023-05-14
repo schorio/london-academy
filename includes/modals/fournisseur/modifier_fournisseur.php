e110e4fd38acd201f078c092df41184cc0c5b847
schorio <schorioignace@gmail.com>
2023-05-14 18:59:37 +0300

    categorie et fournisseur : correction d'orthographe

--- includes/modals/fournisseur/modifier_fournisseur.php
+++ includes/modals/fournisseur/modifier_fournisseur.php
@@ -4,7 +4,7 @@
 			<!-- Modal content-->
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title">Modifier du fournisseur</h5>
+					<h5 class="modal-title">Modifier un fournisseur</h5>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
