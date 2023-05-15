a73aed1ef644895ac220687bdc24a7a8cd8c417b
schorio <schorioignace@gmail.com>
2023-05-15 15:44:28 +0300

    entree : creation du modal de supression

--- includes/modals/entree/supprimer_entree.php
+++ includes/modals/entree/supprimer_entree.php
@@ -1,4 +1,4 @@
-<div id="supprimer_inventaire<?php echo htmlentities($row->id_inv); ?>" class="modal custom-modal fade" role="dialog">
+<div id="supprimer_entree<?php echo htmlentities($row->id_ent); ?>" class="modal custom-modal fade" role="dialog">
 	<div class="modal-dialog modal-dialog-centered">
 		<form method="post">
 			<!-- Modal content-->
@@ -6,17 +6,17 @@
 				<div class="modal-body-delete">
 					<div class="form-header">
 							<button type="button" class="close" data-dismiss="modal">&times;</button>
-							<h3>Supprimer un(e) inventaire</h3>
-							<p>Voulez vous vraiment supprimer l'inventaire <strong><?php echo htmlentities($row->piece_inv); ?></strong> ?</p>
+							<h3>Supprimer un entrée de produit</h3>
+							<p>Voulez vous vraiment supprimer l'entree de <strong><?php echo htmlentities($row->piece_ent); ?></strong> ?</p>
 					</div>
 					<div class="modal-btn delete-action">
-						<input type="hidden" name="supprimer_id" value="<?php echo htmlentities($row->id_inv); ?>">
+						<input type="hidden" name="supprimer_id" value="<?php echo htmlentities($row->id_ent); ?>">
 						<div class="row">
 							<div class="col-6">
 								<a href="javascript:void(0);" data-dismiss="modal" class="btn btn-primary cancel-btn">Annuler</a>
 							</div>
 							<div class="col-6">
-								<button type="submit" name="supprimer_inv" class="btn btn-primary continue-btn">Supprimer</button>
+								<button type="submit" name="supprimer_ent" class="btn btn-primary continue-btn">Supprimer</button>
 							</div>
 						</div>
 					</div>
