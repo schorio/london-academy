4ddad5a93226e15aa1aa9f3629dd42f3e2ec3697
schorio <schorioignace@gmail.com>
2023-05-15 15:40:58 +0300

    entree : creation du modal d'ajout

--- includes/modals/entree/ajouter_entree.php
+++ includes/modals/entree/ajouter_entree.php
@@ -1,8 +1,8 @@
-<div id="ajouter_inventaire" class="modal custom-modal fade" role="dialog">
-	<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+<div id="ajouter_entree" class="modal custom-modal fade" role="dialog">
+	<div class="modal-dialog modal-dialog-centered" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title">Ajouter un(e) inventaire</h5>
+				<h5 class="modal-title">Ajouter un(e) entree</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
@@ -13,80 +13,59 @@
 					<div class="row">
 						<div class="col-sm-12">
 							<div class="form-group">
-								<label for="piece_inv">Nom du produit : </label>
-								<input type="text" class="form-control" id="piece_inv" name="piece_inv" required>
-							</div>
-						</div>
-						<div class="col-sm-6">
-							<div class="form-group">
-								<label for="fournisseur_inv">Fournisseur : </label>
-								<select id="fournisseur_inv" required name="fournisseur_inv" class="select action">
-									<option>Selectionner le fournisseur </option>
+								<label for="piece_ent">Produit : </label>
+								<select id="piece_ent" required name="piece_ent" class="select action">
+									<option>Selectionner le produit </option>
 									<?php 
-										$sql2 = "SELECT * from fournisseur";
+										$sql2 = "SELECT * from inventaire";
 										$query2 = $dbh -> prepare($sql2);
 										$query2->execute();
 										$result2=$query2->fetchAll(PDO::FETCH_OBJ);
-										foreach($result2 as $row)
+										foreach($result2 as $row_inv)
 										{          
 									?>  
-										<option value="<?php echo htmlentities($row->id_frn);?>">
-									<?php echo htmlentities($row->libelle_frn);?></option>
+										<option value="<?php echo htmlentities($row_inv->id_inv);?>">
+									<?php echo htmlentities($row_inv->piece_inv);?></option>
 									<?php
 										} 
 									?> 
 								</select>
-							</div>
-						</div>
-						<div class="col-sm-6">
-							<div class="form-group">
-								<label for="categorie_inv">Categorie : </label>
-								<select id="categorie_inv" required name="categorie_inv" class="select action">
-									<option>Selectionner le categorie </option>
-									<?php 
-										$sql2 = "SELECT * from categorie";
-										$query2 = $dbh -> prepare($sql2);
-										$query2->execute();
-										$result2=$query2->fetchAll(PDO::FETCH_OBJ);
-										foreach($result2 as $row)
-										{          
-									?>  
-										<option value="<?php echo htmlentities($row->id_cat);?>">
-									<?php echo htmlentities($row->libelle_cat);?></option>
-									<?php
-										} 
-									?> 
-								</select>
-							</div>
-						</div>
-						<div class="col-sm-12">
-							<div class="form-group">
-								<label for="description_inv">Description : </label>
-								<input type="text" class="form-control" id="description_inv" name="description_inv" autofocus>
 							</div>
 						</div>
 						<div class="col-sm-9">
 							<div class="form-group">
-								<label for="si_inv">Stock initial</label>
-								<input type="number" class="form-control" id="si_inv" name="si_inv" value="0" autofocus>
+								<label for="reference_ent">Reference : </label>
+								<input type="text" class="form-control" id="reference_ent" name="reference_ent" autofocus>
 							</div>
 						</div>
 						<div class="col-sm-3">
 							<div class="form-group">
-								<label for="sa_inv">actuel : </label>
-								<input type="text" class="form-control" id="sa_inv" name="sa_inv" value="0" readonly>
+								<label for="quantite_ent">Quantite :</label>
+								<input type="number" class="form-control" id="quantite_ent" name="quantite_ent" value="0" autofocus>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+								<label for="pu_ent">Prix unitaire : </label>
+								<input type="text" class="form-control" id="pu_ent" name="pu_ent" value="0" autofocus>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+								<label for="montant_ent">Montant : </label>
+								<input type="text" class="form-control" id="montant_ent" name="montant_ent" autofocus>
 							</div>
 						</div>
 						<div class="col-sm-12">
 							<div class="form-group">
-								<label for="stockage_inv">Stockage : </label>
-								<input type="text" class="form-control" id="stockage_inv" name="stockage_inv" autofocus>
+								<label for="date_ent">Date d'entrée : </label>
+								<input type="date" class="form-control" id="date_ent" name="date_ent" autofocus>
 							</div>
 						</div>
 					</div>
 				
 					<div class="submit-section">
-						<button name="ajouter_inv" type="POST" class="btn btn-primary submit-btn">Valider</button>
+						<button name="ajouter_ent" type="POST" class="btn btn-primary submit-btn">Valider</button>
 					</div>
 				</form>
 			</div>
