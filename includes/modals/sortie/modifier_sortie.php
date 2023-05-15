457d6efa6821312451eac357ea9d5972f5fcc874
schorio <schorioignace@gmail.com>
2023-05-15 17:15:54 +0300

    sortie : creation du modal de modification

--- includes/modals/sortie/modifier_sortie.php
+++ includes/modals/sortie/modifier_sortie.php
@@ -1,4 +1,4 @@
-<div id="modifier_sortie<?php echo htmlentities($row->id_ent); ?>" class="modal custom-modal fade" role="dialog">
+<div id="modifier_sortie<?php echo htmlentities($row->id_sort); ?>" class="modal custom-modal fade" role="dialog">
 	<div class="modal-dialog modal-dialog-centered modal-lg">
 		<!-- Modal content-->
 		<div class="modal-content">
@@ -10,13 +10,13 @@
 					</button>
 				</div>
 				<div class="modal-body">
-					<input type="hidden" name="modifier_id" value="<?php echo htmlentities($row->id_ent); ?>">
+					<input type="hidden" name="modifier_id" value="<?php echo htmlentities($row->id_sort); ?>">
 					<div class="row">
 						<div class="col-sm-12">
 							<div class="form-group">
-								<label for="piece_ent">Produit : </label>
-								<select name="n_piece_ent" class="select action">
-									<option value="<?php echo htmlentities($row->piece_ent); ?>"><?php echo htmlentities($row->piece_ent); ?></option>
+								<label for="piece_sort">Produit : </label>
+								<select name="n_piece_sort" class="select action">
+									<option value="<?php echo htmlentities($row->piece_sort); ?>"><?php echo htmlentities($row->piece_sort); ?></option>
 									<?php 
 										$sql2 = "SELECT * from inventaire";
 										$query2 = $dbh -> prepare($sql2);
@@ -26,7 +26,8 @@
 										{          
 									?>  
 										<option value="<?php echo htmlentities($row_inv->id_inv);?>">
-									<?php echo htmlentities($row_inv->piece_inv);?></option>
+											<?php echo htmlentities($row_inv->piece_inv);?>
+										</option>
 									<?php
 										} 
 									?> 
@@ -35,44 +36,48 @@
 						</div>
 						<div class="col-sm-9">
 							<div class="form-group">
-								<label for="reference_ent">Reference : </label>
-								<input type="text" class="form-control" id="reference_ent" name="n_reference_ent" value="<?php echo htmlentities($row->reference_ent); ?>" autofocus>
+								<label for="technicien_sort">Technicien : </label>
+								<select name="n_technicien_sort" class="select action">
+									<option value="<?php echo htmlentities($row->technicien_sort); ?>"><?php echo htmlentities($row->technicien_sort); ?></option>
+									<?php 
+										$sql2 = "SELECT * from technicien";
+										$query2 = $dbh -> prepare($sql2);
+										$query2->execute();
+										$result2=$query2->fetchAll(PDO::FETCH_OBJ);
+										foreach($result2 as $row_tech)
+										{          
+									?>  
+										<option value="<?php echo htmlentities($row_tech->id_tech);?>">
+											<?php echo htmlentities($row_tech->libelle_tech);?>
+										</option>
+									<?php
+										} 
+									?> 
+								</select>
 							</div>
 						</div>
 						<div class="col-sm-3">
 							<div class="form-group">
-								<label for="quantite_ent">Quantite :</label>
-								<input type="number" class="form-control" id="quantite_ent" name="n_quantite_ent" value="<?php echo htmlentities($row->quantite_ent); ?>" autofocus>
-							</div>
-						</div>
-						<div class="col-sm-6">
-							<div class="form-group">
-								<label for="pu_ent">Prix unitaire : </label>
-								<input type="text" class="form-control" id="pu_ent" name="n_pu_ent" value="<?php echo htmlentities($row->pu_ent); ?>" autofocus>
-							</div>
-						</div>
-						<div class="col-sm-6">
-							<div class="form-group">
-								<label for="montant_ent">Montant : </label>
-								<input type="text" class="form-control" id="montant_ent" name="n_montant_ent" value="<?php echo htmlentities($row->montant_ent); ?>" autofocus>
+								<label for="quantite_sort">Quantite :</label>
+								<input type="number" class="form-control" id="quantite_sort" name="n_quantite_sort" value="<?php echo htmlentities($row->quantite_sort); ?>" autofocus>
 							</div>
 						</div>
 						<div class="col-sm-3">
 							<div class="form-group">
-								<label for="date_ent">Date d'entrée : </label>
-								<input type="date" class="form-control" id="date_ent" name="n_date_ent" value="<?php echo htmlentities($row->date_ent); ?>" autofocus>
+								<label for="date_sort">Date de sortie : </label>
+								<input type="date" class="form-control" id="date_sort" name="n_date_sort" value="<?php echo htmlentities($row->date_sort); ?>" autofocus>
 							</div>
 						</div>
 						<div class="col-sm-9">
 							<div class="form-group">
-								<label for="observation_ent">Observation : </label>
-								<input type="text" class="form-control" id="observation_ent" name="n_observation_ent" value="<?php echo htmlentities($row->observation_ent); ?>" autofocus>
+								<label for="observation_sort">Observation : </label>
+								<input type="text" class="form-control" id="observation_sort" name="n_observation_sort" value="<?php echo htmlentities($row->observation_sort); ?>" autofocus>
 							</div>
 						</div>
 					</div>
 					
 					<div class="submit-section">
-						<button type="submit" class="btn btn-primary submit-btn" name="modifier_ent">Valider</button>
+						<button type="submit" class="btn btn-primary submit-btn" name="modifier_sort">Valider</button>
 					</div>
 				</div>
 			</form>
