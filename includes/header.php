d2114c38ba0289e0990805c892daafaca468d835
schorio <schorioignace@gmail.com>
2023-05-22 19:52:20 +0300

    header : fix lien vers parametre

--- includes/header.php
+++ includes/header.php
@@ -34,109 +34,6 @@
 	
 	<!-- Header Menu -->
 	<ul class="nav user-menu">
-	
-		<!-- Search -->
-		<!-- <li class="nav-item">
-			<div class="top-nav-search">
-				<a href="javascript:void(0);" class="responsive-search">
-					<i class="fa fa-search"></i>
-				</a>
-				<form action="#">
-					<input class="form-control" type="text" placeholder="Rechercher ici . . .">
-					<button class="btn" type="submit"><i class="fa fa-search"></i></button>
-				</form>
-			</div>
-		</li> -->
-		<!-- /Search -->
-	
-		
-	
-		<!-- Notifications -->
-		<!-- <li class="nav-item dropdown">
-			<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-				<i class="fa fa-bell-o"></i> <span class="badge badge-pill">3</span>
-			</a>
-			<div class="dropdown-menu notifications">
-				<div class="topnav-dropdown-header">
-					<span class="notification-title">Notifications</span>
-					<a href="javascript:void(0)" class="clear-noti"> Effacer tout </a>
-				</div>
-				<div class="noti-content">
-					<ul class="notification-list">
-						<li class="notification-message">
-							<a href="#">
-								<div class="media">
-									<span class="avatar">
-										<img alt="" src="assets/img/profiles/avatar-02.jpg">
-									</span>
-									<div class="media-body">
-										<p class="noti-details"><span class="noti-title">John Doe</span> added new task <span class="noti-title">Patient appointment booking</span></p>
-										<p class="noti-time"><span class="notification-time">4 mins ago</span></p>
-									</div>
-								</div>
-							</a>
-						</li>
-						<li class="notification-message">
-							<a href="#">
-								<div class="media">
-									<span class="avatar">
-										<img alt="" src="assets/img/profiles/avatar-03.jpg">
-									</span>
-									<div class="media-body">
-										<p class="noti-details"><span class="noti-title">Tarah Shropshire</span> changed the task name <span class="noti-title">Appointment booking with payment gateway</span></p>
-										<p class="noti-time"><span class="notification-time">6 mins ago</span></p>
-									</div>
-								</div>
-							</a>
-						</li>
-						<li class="notification-message">
-							<a href="#">
-								<div class="media">
-									<span class="avatar">
-										<img alt="" src="assets/img/profiles/avatar-06.jpg">
-									</span>
-									<div class="media-body">
-										<p class="noti-details"><span class="noti-title">Misty Tison</span> added <span class="noti-title">Domenic Houston</span> and <span class="noti-title">Claire Mapes</span> to project <span class="noti-title">Doctor available module</span></p>
-										<p class="noti-time"><span class="notification-time">8 mins ago</span></p>
-									</div>
-								</div>
-							</a>
-						</li>
-						<li class="notification-message">
-							<a href="#">
-								<div class="media">
-									<span class="avatar">
-										<img alt="" src="assets/img/profiles/avatar-17.jpg">
-									</span>
-									<div class="media-body">
-										<p class="noti-details"><span class="noti-title">Rolland Webber</span> completed task <span class="noti-title">Patient and Doctor video conferencing</span></p>
-										<p class="noti-time"><span class="notification-time">12 mins ago</span></p>
-									</div>
-								</div>
-							</a>
-						</li>
-						<li class="notification-message">
-							<a href="#">
-								<div class="media">
-									<span class="avatar">
-										<img alt="" src="assets/img/profiles/avatar-13.jpg">
-									</span>
-									<div class="media-body">
-										<p class="noti-details"><span class="noti-title">Bernardo Galaviz</span> added new task <span class="noti-title">Private chat module</span></p>
-										<p class="noti-time"><span class="notification-time">2 days ago</span></p>
-									</div>
-								</div>
-							</a>
-						</li>
-					</ul>
-				</div>
-				<div class="topnav-dropdown-footer">
-					<a href="#">Voir tout les Notifications</a>
-				</div>
-			</div>
-		</li> -->
-		<!-- /Notifications -->
-		
 
 		<li class="nav-item dropdown has-arrow main-drop">
 			<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
@@ -145,8 +42,7 @@
 				<span><?php echo ucfirst($user_ut); ?></span>
 			</a>
 			<div class="dropdown-menu">
-				<a class="dropdown-item" href="">Mon Profile</a>
-				<a class="dropdown-item" href="">Paramètres</a>
+				<a class="dropdown-item" href="/london-academy/includes/function/utilisateur/changer_mdp.php">Paramètres</a>
 				<a class="dropdown-item" href="/london-academy/includes/function/connection/logout.php">Déconnecter</a>
 			</div>
 		</li>
